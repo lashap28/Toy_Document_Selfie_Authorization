a41b65993186b6c2d72a2c8daf548783c0e76f66
Lpantskhava <Cshk859253>
2022-05-09 01:32:47 +0400

    test commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from kivy.uix.relativelayout import RelativeLayout
+
+class SecondWidget(RelativeLayout):
+    pass
+
+class MainWidget(RelativeLayout):
+    menu_widget = ObjectProperty()
+    counter = 0
+
+    def __init__(self,**kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update, 1/60)
+
+    def update(self, dt):
+        self.counter += 1
+        if self.counter > 130:
+            self.menu_widget.opacity /= 1.006
+        if self.counter > 180:
+            self.menu_widget.opacity = 0
+
+
+
+class testApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    testApp().run()
